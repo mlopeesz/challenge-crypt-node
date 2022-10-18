4c3c434ff019730b908e26c4242f2332c8493672
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-18 08:26:01 -0300

    fix: :bug: fix encrypt and decrypt functions

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,19 +1,23 @@
 const crypto = require('crypto');
 
-const algorithm = 'aes-256-ctr';
-const key = 'facilefacilefacilefacilefacilefa';
-const iv = Buffer.from(crypto.randomBytes(16));
+const algorithm = 'aes-256-cbc';
+const key = 'facilefacilefacilefacilefacilefa'; // Key exposta para fins de avaliação.
 
 const encrypt = (text) => {
+  const iv = Buffer.from(crypto.randomBytes(16));
   const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-  const encryptedText = cipher.update(text, 'utf8', 'hex');
-  return encryptedText;
+  let encryptedText = cipher.update(text);
+  encryptedText = Buffer.concat([encryptedText, cipher.final()]);
+  return `${iv.toString('hex')}:${encryptedText.toString('hex')}`;
 };
 
 const decrypt = (text) => {
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  const decryptedText = decipher.update(text, 'hex', 'utf8');
-  return decryptedText;
+  const [iv, encryptedText] = text.split(':');
+  const ivBuffer = Buffer.from(iv, 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), ivBuffer);
+  let decryptedText = decipher.update(Buffer.from(encryptedText, 'hex'));
+  decryptedText = Buffer.concat([decryptedText, decipher.final()]);
+  return decryptedText.toString();
 };
 
 module.exports = { encrypt, decrypt };
